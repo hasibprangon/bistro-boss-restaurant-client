1004e63143fb3fb1fefd7e47c55e4579ca31c80b
hasibprangon <hasibulhossainp09@gmail.com>
2025-01-08 17:00:05 +0600

    menu section added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../../Shared/Cover/Cover';
-import img from '../../../assets/menu/banner3.jpg'
-import PopularMenu from '../../PopularMenu/PopularMenu';
+import img from '../../../assets/menu/banner3.jpg';
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg';
+import saladImg from '../../../assets/menu/salad-bg.jpg';
+import soupImg from '../../../assets/menu/soup-bg.jpg';
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const soup = menu.filter(item => item.category === 'soup')
+    const dessert = menu.filter(item => item.category === 'dessert')
+    const salad = menu.filter(item => item.category === 'salad')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const offered = menu.filter(item => item.category === 'offered')
+
     return (
         <div>
             <Helmet>
                 <title>Menu || Bistro Boss Restaurant</title>
             </Helmet>
             <Cover img={img} title={'Our Menu'} ></Cover>
-           
+            <SectionTitle subHeading={"Don't Miss"} heading={"Today's Offer"}></SectionTitle>
+            {/* offered */}
+            <MenuCategory items={offered}></MenuCategory>
+
+            {/* dessert */}
+            <MenuCategory items={dessert} title={"Dessert"} coverImg={dessertImg}></MenuCategory>
+
+            {/* pizza */}
+            <MenuCategory items={pizza} title={"Pizza"} coverImg={pizzaImg}></MenuCategory>
+
+            {/* salad */}
+            <MenuCategory items={salad} title={"Salad"} coverImg={saladImg}></MenuCategory>
+
         </div>
     );
 };
--- src/Shared/MenuItem/MenuItem.jsx
+++ src/Shared/MenuItem/MenuItem.jsx
@@ -4,7 +4,7 @@ const MenuItem = ({ item }) => {
     const { name, image, price, recipe } = item;
     return (
         <div className='flex space-x-4'>
-            <img style={{borderRadius: '0 200px 200px 200px'}} className='w-[100px] border-t-8 border-l-8 border-blue-300' src={image} alt="" />
+            <img style={{borderRadius: '0 200px 200px 200px'}} className='w-[100px] border-t-8 border-l-8 border-blue-300 transform transition-transform duration-300 hover:scale-110' src={image} alt="" />
             <div>
                 <h3 className='uppercase'>{name} -------</h3>
                 <p>{recipe}</p>
